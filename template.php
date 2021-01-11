858da37b639a395524a047c5dd1ab8b7170e1557
19036849 <19036849@rp.edu.sg>
2021-01-11 20:13:30 +0800

    Updated 8

---
+++ template.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Bootstrap 4 Template</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
+        <link href="css/all.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+        
+        <style>
+        </style>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                
+            })
+        </script>
+
+    </head>
+    <body>
+        
+        <!-- comment -->
+        
+    </body>
+</html>
